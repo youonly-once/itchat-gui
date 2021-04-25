09e4e07fbf71d7f05b07c3874feef4206d062937
SXS-PRIVATE <15723468981@163.com>
2021-04-25 12:07:14 +0800

    打包配置

--- AdminServer/src/main/java/cn/shu/AdminServerStater.java
+++ AdminServer/src/main/java/cn/shu/AdminServerStater.java
@@ -1,7 +1,8 @@
+package cn.shu;
+
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 
