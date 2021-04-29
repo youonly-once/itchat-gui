58aeb2e5cdeb460c9099854423911e791b82501a
SXS-PRIVATE <15723468981@163.com>
2021-04-29 21:33:20 +0800

    打包问题解决

--- EurekaServer/src/main/java/cn/shu/MyBean.java
+++ EurekaServer/src/main/java/cn/shu/MyBean.java
@@ -1,8 +1,22 @@
 package cn.shu;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
 /**
  * @author SXS
  * @since 4/29/2021
  */
-public class MyBean extends BeanPostProcessors {
+@Component
+public class MyBean implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return bean;
+    }
 }
